d7323b17a7fd464111c8debb881e8b0e256fee0e
Haylin Moore <me@haylinmoore.com>
2024-12-22 16:05:19 -0500

    template: default move to sakura

--- src/utils/template.rs
+++ src/utils/template.rs
@@ -8,7 +8,7 @@ const DEFAULT_TEMPLATE: &str = r#"<!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sakura.css/css/sakura.css">
     <title>{{author}} - {{title}}</title>
     <meta name="author" content="{{author}}">
     {{#if description}}
